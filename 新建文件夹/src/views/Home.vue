d70c2f4ba41b655e713c9fdd3420923d4ac27213
xujiayin <xujiayin@jianke.com>
2019-12-10 15:38:46 +0800

    commit

--- 新建文件夹/src/views/Home.vue
+++ 新建文件夹/src/views/Home.vue
@@ -1,24 +1,48 @@
 <template>
-  <div class="ion-page">
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>
-          ZipInfo
-        </ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content class="ion-padding">My App</ion-content>
-  </div>
+  <ion-content class="ion-padding">
+      <ZipSearch v-on:get-zip="getZipInfo"/>
+      <ZipInfo v-bind:info="info"/>
+      <ClearInfo v-bind:info="info" v-on:clear-info="clearInfo"/>
+  </ion-content>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import ZipSearch from "../components/ZipSearch";
+import ZipInfo from "../components/ZipInfo";
+import ClearInfo from "../components/ClearInfo";
 
 export default {
   name: 'home',
   components: {
-    // HelloWorld
+    ZipSearch,
+    ZipInfo,
+    ClearInfo
+  },
+  data() {
+    return {
+      info: null
+    };
+  },
+  methods: {
+    async getZipInfo(zip) {
+      const res = await fetch(`https://api.zippopotam.us/us/${zip}`);
+      if (res.status == 404) {
+        this.showAlert();
+      }
+      this.info = await res.json();
+    },
+    showAlert() {
+      return this.$ionic.alertController
+        .create({
+          header: "Not Valid",
+          message: "Please enter a valid US ZipCode",
+          buttons: ["OK"]
+        })
+        .then(a => a.present());
+    },
+    clearInfo(){
+      this.info = null;
+    }
   }
 }
 </script>
